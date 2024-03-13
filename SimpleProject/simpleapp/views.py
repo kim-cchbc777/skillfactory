77be6b7d0080502be2fed2adc800ccaecbcc0369
kim <kim@yandex.ru>
2024-03-13 19:48:30 +0100

    Simple App 3/12

--- SimpleProject/simpleapp/views.py
+++ SimpleProject/simpleapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 from .models import Product
+from datetime import datetime
+from pprint import pprint
 
 
 class ProductsList(ListView):
@@ -9,6 +11,13 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        pprint(context)
+        return context
 
 
 class ProductDetail(DetailView):
