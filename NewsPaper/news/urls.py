d6dafe06cbb938f0f4f127bd5ae999e76a66f63d
kim <kim@yandex.ru>
2024-03-19 18:47:24 +0100

    news project D3.3

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -5,5 +5,5 @@ from .views import NewsList, NewsDetail
 
 urlpatterns = [
     path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view())
+    path('<int:pk>', NewsDetail.as_view(), name='single_news')
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models.post import Post
+from pprint import pprint
 
 
 class NewsList(ListView):
     model = Post
-    ordering = 'post_time_creation'
+    ordering = '-post_time_creation'
     template_name = 'news.html'
     context_object_name = 'news'
 
@@ -14,3 +15,8 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # pprint(context)
+    #     return context
